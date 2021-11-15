02adb2b0460801208c6f3a82e606afc2abea7e61
Felix Ruess <felix.ruess@roboception.de>
2021-11-15 11:23:08 +0100

    add more extra_data to CameraParam
    * fix line_source
    * add input_mask and output_mask
    * add model_name
    * add auto_exposure_adapting if supplied by device

--- src/publishers/camera_param_publisher.cpp
+++ src/publishers/camera_param_publisher.cpp
@@ -78,6 +78,16 @@ inline rc_common_msgs::msg::KeyValue getKeyValue(const char * key, T value)
   return ret;
 }
 
+inline rc_common_msgs::msg::KeyValue getKeyValueString(const char * key, const std::string &value)
+{
+  rc_common_msgs::msg::KeyValue ret;
+
+  ret.key=key;
+  ret.value=value;
+
+  return ret;
+}
+
 }
 
 void CameraParamPublisher::publish(const rcg::Buffer * buffer, uint32_t, uint64_t pixelformat)
@@ -93,47 +103,69 @@ void CameraParamPublisher::publish(const rcg::Buffer * buffer, uint32_t, uint64_
     param.header.stamp.sec = time / 1000000000ul;
     param.header.stamp.nanosec = time % 1000000000ul;
 
-    // get list of all available IO lines and iterate over them to get their
-    // LineSource values
+    // get list of all available IO lines and iterate over them
+    // to get input/output masks and LineSource values for outputs
 
     std::vector<std::string> lines;
-    rcg::getEnum(nodemap, "ChunkLineSelector", lines, true);
-    for (auto && line : lines) {
-      rcg::setEnum(nodemap, "ChunkLineSelector", line.c_str(), true);
+    rcg::getEnum(nodemap, "LineSelector", lines, true);
+    uint32_t input_mask = 0;
+    uint32_t output_mask = 0;
+    for (size_t i = 0; i < lines.size(); i++)
+    {
+      rcg::setEnum(nodemap, "LineSelector", lines[i].c_str(), true);
+      std::string io = rcg::getEnum(nodemap, "LineMode");
 
-      rc_common_msgs::msg::KeyValue line_source;
-      line_source.key = line;
-      line_source.value = rcg::getEnum(nodemap, "ChunkLineSource", true);
+      if (io == "Input")
+      {
+        input_mask |= 1 << i;
+      }
 
-      param.line_source.push_back(line_source);
+      if (io == "Output")
+      {
+        output_mask |= 1 << i;
+
+        // for output also get LineSource for this image
+        rcg::setEnum(nodemap, "ChunkLineSelector", lines[i].c_str(), true);
+        rc_common_msgs::msg::KeyValue line_source;
+        line_source.key = lines[i];
+        line_source.value = rcg::getEnum(nodemap, "ChunkLineSource", true);
+        param.line_source.push_back(line_source);
+      }
     }
-
     param.line_status_all = rcg::getInteger(nodemap, "ChunkLineStatusAll", 0, 0, true);
 
     param.gain = rcg::getFloat(nodemap, "ChunkGain", 0, 0, true);
     param.exposure_time = rcg::getFloat(nodemap, "ChunkExposureTime", 0, 0, true) / 1000000l;
 
+    param.extra_data.push_back(getKeyValue("noise", rcg::getFloat(nodemap, "ChunkRcNoise", 0, 0, false)));
+    param.extra_data.push_back(getKeyValue("test", rcg::getBoolean(nodemap, "ChunkRcTest", false)));
+    param.extra_data.push_back(getKeyValue("brightness", rcg::getFloat(nodemap, "ChunkRcBrightness", 0, 0, false)));
+    param.extra_data.push_back(getKeyValue("input_mask", input_mask));
+    param.extra_data.push_back(getKeyValue("output_mask", output_mask));
+
+    float out1_reduction = 0;
+    try
     {
-      float noise = rcg::getFloat(nodemap, "ChunkRcNoise", 0, 0, false);
-      bool test = rcg::getBoolean(nodemap, "ChunkRcTest", false);
-
-      float out1_reduction = 0;
-
-      try {
-        out1_reduction = rcg::getFloat(nodemap, "ChunkRcOut1Reduction", 0, 0, true);
-      } catch (const std::exception &) {
-        // can be removed if sensor version must be >= 20.10.1
-        out1_reduction = rcg::getFloat(nodemap, "ChunkRcAdaptiveOut1Reduction", 0, 0, false);
-      }
-
-      float brightness = rcg::getFloat(nodemap, "ChunkRcBrightness", 0, 0, false);
-
-      param.extra_data.clear();
-      param.extra_data.push_back(getKeyValue("noise", noise));
-      param.extra_data.push_back(getKeyValue("test", test));
-      param.extra_data.push_back(getKeyValue("out1_reduction", out1_reduction));
-      param.extra_data.push_back(getKeyValue("brightness", brightness));
+      out1_reduction = rcg::getFloat(nodemap, "ChunkRcOut1Reduction", 0, 0, true);
     }
+    catch (const std::exception&)
+    {
+      // can be removed if sensor version must be >= 20.10.1
+      out1_reduction = rcg::getFloat(nodemap, "ChunkRcAdaptiveOut1Reduction", 0, 0, false);
+    }
+    param.extra_data.push_back(getKeyValue("out1_reduction", out1_reduction));
+
+    try
+    {
+      bool adapting = rcg::getBoolean(nodemap, "ChunkRcAutoExposureAdapting", true);
+      param.extra_data.push_back(getKeyValue("auto_exposure_adapting", adapting));
+    }
+    catch (const std::exception&)
+    {
+      // feature not available on device
+    }
+
+    param.extra_data.push_back(getKeyValueString("model_name", rcg::getString(nodemap, "DeviceModelName", false)));
 
     pub->publish(param);
   }
